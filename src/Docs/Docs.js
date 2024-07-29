93aa44d9df2196e24dae211f188d5da0a8c7a20e
Manohar Poduri <poduri.manohar46@gmail.com>
2024-07-28 21:07:49 -0500

    creating firebase doc

---
+++ src/Docs/Docs.js
@@ -0,0 +1,5 @@
+import { doc } from "firebase/firestore";
+import { db } from "../firebase";
+export const createService = doc(db,"createservices","T7ZjsodKs1CM6in3NyqF");
+export const careservices = doc(db,"careservices","1Xa8LAR4PQUYl8IvzjtJ");
+export const community = doc(db,"community","YhlMTI1R4LJrVOOaDrI9");
